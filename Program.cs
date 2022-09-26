34cdba73f353bbc8a34136bc1a561c2ce3839eaf
sahib2442 <kadersahib1234@gmail.com>
2022-09-26 08:04:07 +0530

    [sahib] Add: pincode

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegexProblemStatement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Validate validate = new Validate();
+            validate.Pincode("400088");
+        }
+        
+    }
+}
---
+++ Validate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegexProblemStatement
+{
+    public class Validate
+    {
+        public const string PINCODE_REGEX_NUM = "^[0-9]{6}$";
+        public const string PINCODE_REGEX = "^([0-9]+([ ][0-9]){0,1}){6,7}$";
+        public void Pincode(string pincode)
+        {
+            Regex regex = new Regex(PINCODE_REGEX_NUM);
+            bool result = regex.IsMatch(pincode);
+            Console.WriteLine(result);
+        }
+        
+    }
+}
